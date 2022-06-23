238b0769d17b0d6659e5ee753e28d7519c5444a2
Jordan Williams <jwysong116@gmail.com>
2022-06-22 20:44:57 -0400

    updated add gifs function

--- popup.js
+++ popup.js
@@ -1,25 +1,46 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("colorDiv");
+let changeColor = document.getElementById('colorDiv');
 
-chrome.storage.sync.get("color", ({ color }) => {
+chrome.storage.sync.get('color', ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
-let giphyAPI = "4lALB2Uo7bfxD6r1IPBZBqejL4G4mDIy"
-let search = "ryan+gosling"
-let giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + giphyAPI + "&limit=5"
+// Gif Container
+let getGifContainer = document.getElementById('gifContainer');
+
+let giphyAPI = '4lALB2Uo7bfxD6r1IPBZBqejL4G4mDIy';
+let search = 'ryan+gosling';
+let giphyURL =
+  'http://api.giphy.com/v1/gifs/search?q=' +
+  search +
+  '&api_key=' +
+  giphyAPI +
+  '&limit=5';
+
+function generateGifs(gifs) {
+  for (let i = 0; i <= data.data.length; i++) {
+    let createGif = document.createElement('video');
+    getGifContainer.appendChild(createGif);
+    createGif.setAttribute('id', `${i}`);
+    createGif.setAttribute('class', 'newGif');
+    createGif.setAttribute('src', `${data.data[i].url}`);
+  }
+
+  console.log(getGifContainer);
+}
 
 // make AJAX call here....
 // starts the process of fetching a resource from the network
 fetch(giphyURL)
-  .then(response => {
+  .then((response) => {
     // submitting request to be formatted in json
-    return response.json() // once promise is resolved, object "data" gets returned out to the next .then()
+    return response.json(); // once promise is resolved, object "data" gets returned out to the next .then()
   })
-  .then(data => {
+  .then((data) => {
     // iterate over the array of objects
     // for (let i = 0; i < data.length; i++) {
     //   newChatRow(data[i].created_by, data[i].message, data[i].created_at);
     // }
-    console.log(data)
-  }); // data 
+    generateGifs(data);
+    console.log(data);
+  });
